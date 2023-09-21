a2810fb11f67bd671990efe479cac866d60db468
SebFoodJ <sebastien.fougere@gmail.com>
2023-09-21 16:46:41 +0200

    modif description

--- src/components/Description.js
+++ src/components/Description.js
@@ -8,14 +8,19 @@ export default function Description() {
                     <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
                         A propos de moi
                     </h1>
-                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-                        Je suis actuellement étudiant en développement web à <a href="https://adatechschool.fr/" target="_blank" className="text-lavender font-bold hidden lg:inline-block title-font">Ada Tech School</a>.<br/>
-                        Je suis à la recherche d'une alternance pour la rentrée 2023.<br/>
-                        Je suis passionné par le développement web et je souhaite en faire mon métier.<br/>
-                        Je suis curieux, autonome et j'aime travailler en équipe.<br/>
-                        Je suis à l'écoute de toutes propositions.<br/>
-                        N'hésitez pas à me contacter pour plus d'informations.<br/>
+                    <div className="italic">
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base mb-3">
+                        "Étudiant à <a href="https://adatechschool.fr/" target="_blank" className="text-lavender font-bold hidden lg:inline-block title-font">Ada Tech School</a>, une école à la pédagogie alternative qui valorise la pratique et les projets collectifs, 
+                        j'ai eu l'opportunité de travailler sur 6 projets collectifs en l'espace de 8 mois. Ces projets, rythmés par des rituels issus de la méthodologie Agile, m'ont permis de développer des compétences essentielles pour le monde professionnel. 
                     </p>
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base mb-3"> 
+                        Au fil de mes expériences professionnelles, j'ai exploré différentes voies, notamment en Événementiel au sein d'une agence de communication, puis en tant que Chef de Cuisine dans des restaurants gastronomiques et bistronomiques .
+                    </p>
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base"> 
+                        Mes réorientations sont le résultat de mon désir constant d'acquérir de nouvelles compétences et de m'adapter à de nouveaux environnements.  
+                        Ma curiosité est mon moteur, et elle m'incite à rechercher en permanence de nouvelles connaissances."
+                    </p>
+                    </div>
                 </div>
             </div>
         </section>
